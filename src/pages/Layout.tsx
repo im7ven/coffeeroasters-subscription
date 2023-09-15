4547998cbc94ce739243cadc500effdfe91a8f2e
Justin <alexanderrr416@gmail.com>
2023-09-14 23:02:38 -0400

    Refactor: Add page padding.

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { GlobalStyles } from "../styles/styles.global";
 
 export const Layout = () => {
   return (
     <>
+      <GlobalStyles />
       <main>
         <Outlet />
       </main>
--- src/styles/styles.global.ts
+++ src/styles/styles.global.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
-const GlobalStyles = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
 html {
     font-size: 62.5%
     scroll-behavior: smooth;
@@ -18,4 +18,14 @@ body {
 }
 `;
 
-export default GlobalStyles;
+export const PagePadding = styled.div`
+  padding: 3.2rem 2.4rem 7.2rem;
+
+  @media screen and (min-width: 768px) {
+    padding: 4rem 4rem 7.2rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    padding: 4.3rem 4rem 8.8rem;
+  }
+`;
