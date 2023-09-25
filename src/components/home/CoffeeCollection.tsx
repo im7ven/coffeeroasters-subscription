5c5be0f6b9d6e3fca199805a3cf8221181ab7e3a
Justin <alexanderrr416@gmail.com>
2023-09-24 21:39:08 -0400

    Build Coffee collection component.

---
+++ src/components/home/CoffeeCollection.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import { baseFont } from "../../styles/styles.typography";
+import { ReactNode } from "react";
+
+const CollectionWrapper = styled.section`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 111.1rem;
+  margin: 12rem auto 3.5rem;
+  gap: 4.3rem;
+
+  @media screen and (min-width: 768px) {
+    margin: 14.4rem auto 0;
+    gap: 4.7rem;
+  }
+
+  @media screen and (min-width: 1240px) {
+    margin: 26.2rem auto 0;
+    gap: 4.7rem;
+    flex-direction: row;
+  }
+`;
+
+const CollectionHeading = styled.h2`
+  ${baseFont}
+  z-index: -1;
+  text-align: center;
+  font-size: 4.2rem;
+  background: -webkit-linear-gradient(
+    270deg,
+    rgba(151, 151, 151, 0.7) 20%,
+    rgba(255, 255, 255, 1) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  margin: 0 0 3.5rem;
+
+  @media screen and (min-width: 500px) {
+    font-size: 6rem;
+  }
+
+  @media screen and (min-width: 768px) {
+    letter-spacing: 0.35rem;
+    position: absolute;
+    top: -9%;
+    font-size: 10.5rem;
+    background: -webkit-linear-gradient(
+      270deg,
+      rgba(151, 151, 151, 0.7) 0%,
+      rgba(255, 255, 255, 1) 85%
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  @media screen and (min-width: 1240px) {
+    font-size: 17.5rem;
+    letter-spacing: 0.5rem;
+    top: -32%;
+  }
+`;
+
+interface Props {
+  children: ReactNode;
+}
+
+export const CoffeeCollection = ({ children }: Props) => {
+  return (
+    <CollectionWrapper>
+      <CollectionHeading>our collection</CollectionHeading>
+      {children}
+    </CollectionWrapper>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,36 @@
+import { CoffeeCollection } from "../components/home/CoffeeCollection";
 import { CoffeeProduct } from "../components/home/CoffeeProduct";
 import { Hero } from "../components/home/Hero";
+import granEspresso from "../images/home/desktop/image-gran-espresso.png";
+import planto from "../images/home/desktop/image-planalto.png";
 import piccollo from "../images/home/desktop/image-piccollo.png";
+import danche from "../images/home/desktop/image-danche.png";
 
 export const HomePage = () => {
   return (
     <>
-      <Hero></Hero>;
-      <br />
-      <br />
-      <br />
-      <br />
-      <CoffeeProduct src={piccollo} alt="Piccollo coffee" heading="Piccollo">
-        Mild and smooth blend featuring notes of toasted almond and dried cherry
-      </CoffeeProduct>
+      <Hero />
+      <CoffeeCollection>
+        <CoffeeProduct
+          src={granEspresso}
+          alt="Gran Espresso coffee"
+          heading="Gran Espresso"
+        >
+          Light and flavorful blend with cocoa and black pepper for an intense
+          experience
+        </CoffeeProduct>
+        <CoffeeProduct src={planto} alt="Planto coffee" heading="Planalto">
+          Brazilian dark roast with rich and velvety body, and hints of fruits
+          and nuts
+        </CoffeeProduct>
+        <CoffeeProduct src={piccollo} alt="Piccollo coffee" heading="Piccollo">
+          Mild and smooth blend featuring notes of toasted almond and dried
+          cherry
+        </CoffeeProduct>
+        <CoffeeProduct src={danche} alt="Danche coffee" heading="Danche">
+          Ethiopian hand-harvested blend densely packed with vibrant fruit notes
+        </CoffeeProduct>
+      </CoffeeCollection>
     </>
   );
 };
