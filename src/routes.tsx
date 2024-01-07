59e34c97840a234348ed6e4220e45bc1eb886d6a
Justin <alexanderrr416@gmail.com>
2024-01-07 14:34:40 -0500

    Implement scroll to top of page for routes.

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,22 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { HomePage } from "./pages/HomePage";
 import { AboutPage } from "./pages/AboutPage";
+import CreatePlanPage from "./pages/CreatePlanPage";
+import ScrollToTop from "./components/shared/ScrollToTop";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <>
+        <ScrollToTop />
+        <Layout />
+      </>
+    ),
     children: [
       { path: "", element: <HomePage /> },
       { path: "/about-us", element: <AboutPage /> },
+      { path: "/create-plan", element: <CreatePlanPage /> },
     ],
   },
 ]);
